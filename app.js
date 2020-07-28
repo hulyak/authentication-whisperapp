460343617840ab749e9896f46c84e7d6bd2afc60
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-28 13:39:07 -0700

    feat: encrypt with salting and hashing

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption")  use md5
-const md5 = require('md5')
+// const md5 = require('md5');  use bcrypt
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+
 const app = express();
 
 app.use(express.static("public"));
@@ -46,25 +50,30 @@ app.get('/register', (req, res) => {
 //LEVEL 1- USER NAME AND PASSWORD ONLY
 //users data added to database check with robo-3t
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password) //irreversible hash
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+         email: req.body.username,
+    // password: md5(req.body.password) //irreversible hash
+          password: hash  //bcrypt + 10 rounds of salting
   });
   //During save, documents are encrypted and then signed(with mongoose encryption)
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets"); //if no error render secrets.ejs, it will only show secrets page, if the user is registered
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets"); //if no error render secrets.ejs, it will only show secrets page, if the user is registered
     }
   });
+});
 });
 
 //login route, check the database if we have their email and password saved inside the database
 app.post("/login", function (req, res) {
   //users input
   const username = req.body.username;
-  const password = md5(req.body.password);
+  // const password = md5(req.body.password);
+  const password = req.body.password;
 
   //check against database, email(database) field with username(user gave)
   //problem is with password, shows as a string, for this use mongoose encryption
@@ -74,9 +83,13 @@ app.post("/login", function (req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {  //check their password
-          res.render('secrets');
-        }
+        // if (foundUser.password === password) {  //check their password
+        // Load hash from your password DB.
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   });
